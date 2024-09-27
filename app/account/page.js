0347401c51900627ee47f734afbd7f2c4cb949ef
Aaliyan Asif <aaliyanasif@outlook.com>
2024-09-27 18:35:46 +0500

    WIP: home

---
+++ app/account/page.js
@@ -0,0 +1,8 @@
+export const metadata = {
+  title:'Account'
+}
+
+
+export default function Page() {
+  return <h1>Your Account</h1>;
+}
---
+++ app/cabins/page.js
@@ -0,0 +1,8 @@
+
+export const metadata = {
+  title:'Cabins'
+}
+
+export default function Page() {
+  return <h1>Cabins Page</h1>;
+}
